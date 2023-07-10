010f660b4a768fd99c441cb8b0282d27cdf2be0e
arshadazaad3 <arshad.azaad32@gmail.com>
2023-07-11 01:58:14 +0530

    updated code

--- app/src/components/BodyContainer/Training.js
+++ app/src/components/BodyContainer/Training.js
@@ -1,7 +1,26 @@
-import * as knnClassifier from "@tensorflow-models/knn-classifier";
-import * as mobilenet from "@tensorflow-models/mobilenet";
-import * as tf from "@tensorflow/tfjs";
-import React, { useEffect, useRef, useState } from "react";
+import * as knnClassifier from '@tensorflow-models/knn-classifier';
+import * as mobilenet from '@tensorflow-models/mobilenet';
+import * as tf from '@tensorflow/tfjs';
+import React, { useEffect, useRef, useState } from 'react';
+
+import sampleModel from './model.json';
+
+const letters = {
+  ah: 'අ',
+  aah: 'ආ',
+  aeh: 'ඇ',
+  ee: 'ඉ',
+  eeh: 'ඊ',
+  uh: 'උ',
+  uhh: 'ඌ',
+  a: 'එ',
+  ae: 'ඒ',
+  o: 'ඔ',
+  oh: 'ඕ',
+  k: 'ක්',
+  ig: 'ග්',
+  t: 'ටී',
+};
 
 const TrainingPage = () => {
   const webcamRef = useRef(null);
@@ -12,50 +31,80 @@ const TrainingPage = () => {
   const confidenceRef = useRef(null);
   const inputClassNameRef = useRef(null);
 
-  let mobilenetModel;
-  let knnClassifierModel;
-  let webcamInput;
+  const [mobileNetModel, setMobileNetModel] = useState(null);
+  const [knnClassifierModel, setKnnClassifierModel] = useState(null);
+  const [webcamInput, setWebcamInput] = useState(null);
+
+  const [predictedLabel, setPredictedLabel] = useState(null);
+  const [label, setLabel] = useState('');
 
   useEffect(() => {
     const createKNNClassifier = async () => {
-      console.log("Loading KNN Classifier");
+      console.log('Loading KNN Classifier');
       return await knnClassifier.create();
     };
 
     const createMobileNetModel = async () => {
-      console.log("Loading Mobilenet Model");
+      console.log('Loading Mobilenet Model');
       return await mobilenet.load();
     };
 
     const createWebcamInput = async () => {
-        console.log('Loading Webcam Input');
-        const webcamElement = webcamRef.current;
-        return await tf.data.webcam(webcamElement);
-      };
-  
-    const initializeModels = async () => {
-      mobilenetModel = await createMobileNetModel();
-      knnClassifierModel = await createKNNClassifier();
-      webcamInput = await createWebcamInput();
-      imageClassificationWithTransferLearningOnWebcam();
+      console.log('Loading Webcam Input');
+      const webcamElement = webcamRef.current;
+      return await tf.data.webcam(webcamElement);
     };
+
+    const initializeModels = async () => {
+      setMobileNetModel(await createMobileNetModel());
+      setKnnClassifierModel(await createKNNClassifier());
+      setWebcamInput(await createWebcamInput());
+    };
+
     const startWebcam = () => {
-        navigator.mediaDevices.getUserMedia({ video: true })
-          .then((stream) => {
-            const video = webcamRef.current;
-            video.srcObject = stream;
-            video.play();
-          })
-          .catch((error) => {
-            console.error('Error accessing webcam:', error);
-          });
+      navigator.mediaDevices
+        .getUserMedia({ video: true })
+        .then((stream) => {
+          const video = webcamRef.current;
+          video.srcObject = stream;
+          video.play();
+        })
+        .catch((error) => {
+          console.error('Error accessing webcam:', error);
+        });
     };
 
     startWebcam();
     initializeModels();
-  },[]);
+  }, []);
 
-  
+  useEffect(() => {
+    if (mobileNetModel && knnClassifierModel && webcamInput) {
+      imageClassificationWithTransferLearningOnWebcam();
+    }
+  }, [mobileNetModel, knnClassifierModel, webcamInput]);
+
+  // side effect to load model
+  useEffect(() => {
+    (async () => {
+      if (!knnClassifierModel || !mobileNetModel || !webcamInput) {
+        return;
+      }
+      setUploadedModel(true);
+      const loadedModel = sampleModel;
+
+      Object.keys(loadedModel).forEach((key) => {
+        loadedModel[key] = tf.tensor(loadedModel[key], [
+          loadedModel[key].length / 1024,
+          1024,
+        ]);
+        setClasses((prevClasses) => [...prevClasses, key]);
+      });
+      knnClassifierModel.setClassifierDataset(loadedModel);
+      console.log('classifier has been set up!');
+    })();
+  }, [knnClassifierModel, mobileNetModel, webcamInput]);
+
   const addClass = () => {
     const inputClassName = inputClassNameRef.current.value;
     const found = classes.some((el) => el.name === inputClassName);
@@ -66,7 +115,7 @@ const TrainingPage = () => {
         { id: identity + 1, name: inputClassName, count: 0 },
       ]);
     }
-    inputClassNameRef.current.value = "";
+    inputClassNameRef.current.value = '';
   };
 
   const saveClassifier = (classifierModel) => {
@@ -79,10 +128,10 @@ const TrainingPage = () => {
       i += 1;
     });
     let jsonModel = JSON.stringify(datasetObject);
-    let downloader = document.createElement("a");
-    downloader.download = "model.json";
+    let downloader = document.createElement('a');
+    downloader.download = 'model.json';
     downloader.href =
-      "data:text/text;charset=utf-8," + encodeURIComponent(jsonModel);
+      'data:text/text;charset=utf-8,' + encodeURIComponent(jsonModel);
     document.body.appendChild(downloader);
     downloader.click();
     downloader.remove();
@@ -105,7 +154,7 @@ const TrainingPage = () => {
           setClasses((prevClasses) => [...prevClasses, key]);
         });
         classifierModel.setClassifierDataset(tensorObj);
-        console.log("Classifier has been set up! Congrats! ");
+        console.log('Classifier has been set up! Congrats! ');
       };
     }
     await fr.readAsText(inputModel[0]);
@@ -117,7 +166,7 @@ const TrainingPage = () => {
 
   const addDatasetClass = async (classId) => {
     const img = await webcamInput.capture();
-    const activation = mobilenetModel.infer(img, "conv_preds");
+    const activation = mobileNetModel.infer(img, 'conv_preds');
     knnClassifierModel.addExample(activation, classId);
 
     let classIndex = classes.findIndex((el) => el.id === classId);
@@ -132,34 +181,13 @@ const TrainingPage = () => {
     img.dispose();
   };
   const imageClassificationWithTransferLearningOnWebcam = async () => {
-    console.log("Machine Learning on the web is ready");
     while (true) {
       if (knnClassifierModel.getNumClasses() > 0) {
         const img = await webcamInput.capture();
-        const activation = mobilenetModel.infer(img, "conv_preds");
+        const activation = mobileNetModel.infer(img, 'conv_preds');
         const result = await knnClassifierModel.predictClass(activation);
-        console.log(result);
-     
-        if (uploadedModel) {
-          predictionsRef.current.innerHTML = result.label;
-          confidenceRef.current.innerHTML = Math.floor(
-            result.confidences[result.label] * 100
-          );
-        } else {
-          try {
-            console.log('IMMMM2');
-
-            predictionsRef.current.innerHTML = classes[result.label - 1].name;
-            confidenceRef.current.innerHTML = Math.floor(
-              result.confidences[result.label] * 100
-            );
-          } catch (err) {
-            console.log('IMMMM');
-            predictionsRef.current.innerHTML = result.label - 1;
-            confidenceRef.current.innerHTML = Math.floor(
-              result.confidences[result.label] * 100
-            );
-          }
+        if (predictedLabel !== result?.label) {
+          setPredictedLabel(result?.label);
         }
         img.dispose();
       }
@@ -167,59 +195,59 @@ const TrainingPage = () => {
     }
   };
 
-  const speakPrediction = () => {
-    var msg = new SpeechSynthesisUtterance();
-    msg.text = predictionsRef.current.innerHTML;
-    window.speechSynthesis.speak(msg);
-  };
+  function handleSaveInput() {
+    if (predictedLabel) {
+      setLabel(label + letters[predictedLabel]);
+    }
+  }
+
+  function handleSpaceInput() {
+    setLabel(label + ' ');
+  }
+
+  function handleDeleteInput() {
+    if (label && label.length > 0) {
+      setLabel(label.substring(0, label.length - 1));
+    }
+  }
+
+  function handleClearInput() {
+    setLabel('');
+  }
+
+  function formatPredictedLabel(label) {
+    if (!label) {
+      return '';
+    }
+    return letters[label] || '';
+  }
 
   return (
-    <div>
-      <video ref={webcamRef} autoPlay></video>
-      <div id="training-cards">
-        {classes.map((classItem) => (
-          <div className="newshifter" key={classItem.id}>
-            <div className="text-center">
-              <h3>
-                Class Name : <span>{classItem.name}</span>
-              </h3>
-              <h3>
-                Images :{" "}
-                <span id={`images-${classItem.id}`}>{classItem.count}</span>
-              </h3>
-            </div>
-            <div>
-              <button
-                className="dark btn-spread btn-shadow mr-5"
-                id={classItem.id}
-                onClick={() => addDatasetClass(classItem.id)}
-              >
-                Add New Images <i className="fas fa-plus fa-1x"></i>
-              </button>
-            </div>
-          </div>
-        ))}
+    <div className="web_cam__container">
+      <video ref={webcamRef} autoPlay className="web_cam__video" />
+      <h1 className="predicted_label__title">Predicted Label</h1>
+      {predictedLabel && (
+        <div className="predicted_label__container">
+          <h3>{formatPredictedLabel(predictedLabel)}</h3>
+        </div>
+      )}
+      <br />
+      <input value={label} className="label__input" disabled />
+
+      <div className="button_row">
+        <button className="input_btn" onClick={handleSaveInput}>
+          Save
+        </button>
+        <button className="input_btn" onClick={handleSpaceInput}>
+          Space
+        </button>
+        <button className="input_btn" onClick={handleDeleteInput}>
+          Delete
+        </button>
+        <button className="input_btn" onClick={handleClearInput}>
+          Clear
+        </button>
       </div>
-      <div id="predictions" ref={predictionsRef}></div>
-      <div id="confidence" ref={confidenceRef}></div>
-      <input type="text" id="inputClassName" ref={inputClassNameRef} />
-      <button id="add-button" onClick={addClass}>
-        Add Class
-      </button>
-      <button id="btnSpeak" onClick={speakPrediction}>
-        Speak Prediction
-      </button>
-      <input
-        type="file"
-        id="load_button"
-        onChange={(event) => uploadModel(knnClassifierModel, event)}
-      />
-      <button
-        id="save_button"
-        onClick={() => downloadModel(knnClassifierModel)}
-      >
-        Download Model
-      </button>
     </div>
   );
 };
